0acd12d5dde6888f0971e3123496ea0464309007
diogo <diogomarta10@gmail.com>
2021-05-24 17:14:52 +0100

    eliminate problems

--- email_sender/email_sender/urls.py
+++ email_sender/email_sender/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
+    path('',include('mail.urls')),
     path('admin/', admin.site.urls),
-    path('mail',include('mail.urls')),
 ]
