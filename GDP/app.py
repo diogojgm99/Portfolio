259a7a4a7dd6e3d18f7e9073d804880aeeccd743
diogo <diogomarta10@gmail.com>
2021-07-20 12:18:26 +0100

    Added the first menu and function to get gdp over selected years

--- GDP/app.py
+++ GDP/app.py
@@ -1,7 +1,30 @@
+from os import read
 import pandas as pd
 import csv
 
-with open('data1.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+def gdp_over_years(reader,country,first_year,last_year):
+    year = first_year
     for row in reader:
-        print(row[str(1960)])
+        if country == row['Country Name']:
+            while (year <= last_year):
+                print(f'{str(year)} : {row[str(year)]}')
+                year +=1;
+                
+
+
+if __name__ == "__main__":
+    #reading CSV file
+    with open('data1.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        # gdp_over_years(reader,'Belize',1960,1965)
+        while True:
+            country = input("Insert the selected country: ")
+            print(country)
+            initial_year = int(input("Insert the initial year: "))
+            print(initial_year)
+            final_year = int(input("Insert the initial year: "))
+            print(final_year)
+            gdp_over_years(reader,country,initial_year,final_year)
+            break
+
+        
